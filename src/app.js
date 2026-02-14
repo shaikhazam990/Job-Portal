e170bc91eff72be546e99fe1a66d8c0faf250100
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-14 11:46:46 +0530

    connected to db

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require("express")
+const cookieparser = require("cookie-parser")
+const cors = require("cors")
 
 const app = express()
+app.use(express.json())
+app.use(cookieparser)
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
 
 module.exports = app
